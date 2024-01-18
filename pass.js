7a988036b165b844719970731c54d756e8a15e60
teamswafa <157039034+teamswafa@users.noreply.github.com>
2024-01-18 20:59:22 +0530

    added new files

---
+++ pass.js
@@ -0,0 +1,16 @@
+const inputBox = document.getElementById("input")
+const button = document.getElementById("btn")
+const error = document.getElementById("result")
+const result = document.getElementById("link")
+
+function checkpassword(){
+    if( inputBox.value===''){
+        alert("you must write your password")}
+    if(inputBox.value === 'swafaianz'){
+        result.outerHTML = 'welcome'
+        }
+        
+}
+    
+
+                                            
